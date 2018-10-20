46f760cb5b8e3e70d5c47c57515a005819563ed0
Myles Tyson <22473224+mylestyson@users.noreply.github.com>
2018-10-20 12:08:57 +0100

    added reading in of urls, in process of adding downloading

--- data-scraper/scraper.py
+++ data-scraper/scraper.py
@@ -1,9 +1,26 @@
 ##import resources
 import urllib
+import urllib.request
+from urllib.parse import urlparse
+import os
 
-def: main()
-    
+def main():
+    getFiles()
 
+def getFiles():
+    ## Reading in links
+    path = "HDFEOS-urls.txt"
+    file = open(path,"r")
+    urls = file.readlines()
+
+
+    # https://n5eil01u.ecs.nsidc.org/DP5/OTHR/NISE.004/2017.04.30/NISE_SSMISF17_20170430.HDFEOS
+
+    for url in urls:
+        splits = url.split("/")
+        filename = splits[len(splits) - 1]
+        print(filename)
+        urllib.request.urlretrieve(url, 'data/' + filename)
 
 
 
