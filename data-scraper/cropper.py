3cc0506cd9460dcc43f51619506d13b7fc19d954
Myles Tyson <22473224+mylestyson@users.noreply.github.com>
2018-10-21 09:04:58 +0100

    Data folders now included in commit (apart from the 6gb of HDFEOS)

--- data-scraper/cropper.py
+++ data-scraper/cropper.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 
 def main():
-   inPath = os.path.join("data", "csv")
-   outPath = os.path.join("data", "out")
+   inPath = "data" + "\\" + "csv"
+   outPath = "data" + "\\" + "smallcsv"
    names = []
    for file in os.listdir(inPath):
       with open(os.path.join(inPath, file),'r') as csv_file:
@@ -15,10 +15,12 @@ def main():
          lines = []
          for row in csv_reader:
             fileLines.append(row)
-         for i in range (220,510):
-            lines.append(fileLines[i][220:510])
+         for i in range (200,490):
+            lines.append(fileLines[i][200:490])
          df = pd.DataFrame(lines)
-         df.to_csv(os.path.join(outPath, file), index = False)
+         df.to_csv(outPath + "\\" + file, index = False)
+         #input("derp")
+
 
 if __name__ == '__main__':
    main()
